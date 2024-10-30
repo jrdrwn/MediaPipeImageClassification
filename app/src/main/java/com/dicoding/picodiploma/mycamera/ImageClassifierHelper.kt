7e9d3fbc772de72b9fe2662db55c4277c6b1cd42
Jordi Irawan <64052045+jrdrwn@users.noreply.github.com>
2024-10-30 23:40:37 +0700

    Latihan ImageClassifier dengan MediaPipe

---
+++ app/src/main/java/com/dicoding/picodiploma/mycamera/ImageClassifierHelper.kt
@@ -0,0 +1,95 @@
+package com.dicoding.picodiploma.mycamera
+
+import android.content.Context
+import android.os.SystemClock
+import android.util.Log
+import androidx.camera.core.ImageProxy
+import com.google.mediapipe.framework.image.BitmapImageBuilder
+import com.google.mediapipe.tasks.components.containers.Classifications
+import com.google.mediapipe.tasks.core.BaseOptions
+import com.google.mediapipe.tasks.vision.core.ImageProcessingOptions
+import com.google.mediapipe.tasks.vision.core.RunningMode
+import com.google.mediapipe.tasks.vision.imageclassifier.ImageClassifier
+
+class ImageClassifierHelper(
+    var threshold: Float = 0.1f,
+    var maxResults: Int = 3,
+    val modelName: String = "mobilenet_v1.tflite",
+    val runningMode: RunningMode = RunningMode.LIVE_STREAM,
+    val context: Context,
+    val classifierListener: ClassifierListener?
+) {
+    private var imageClassifier: ImageClassifier? = null
+
+    init {
+        setupImageClassifier()
+    }
+
+    private fun setupImageClassifier() {
+        val optionsBuilder = ImageClassifier.ImageClassifierOptions.builder()
+            .setScoreThreshold(threshold)
+            .setMaxResults(maxResults)
+            .setRunningMode(runningMode)
+
+        if (runningMode == RunningMode.LIVE_STREAM) {
+            optionsBuilder.setResultListener { result, image ->
+                val finishTimeMs = SystemClock.uptimeMillis()
+                val inferenceTime = finishTimeMs - result.timestampMs()
+                classifierListener?.onResults(
+                    result.classificationResult().classifications(),
+                    inferenceTime
+                )
+            }.setErrorListener { error ->
+                classifierListener?.onError(error.message.toString())
+            }
+        }
+
+        val baseOptionsBuilder = BaseOptions.builder()
+            .setModelAssetPath(modelName)
+        optionsBuilder.setBaseOptions(baseOptionsBuilder.build())
+
+        try {
+            imageClassifier = ImageClassifier.createFromOptions(
+                context,
+                optionsBuilder.build()
+            )
+        } catch (e: IllegalStateException) {
+            classifierListener?.onError(context.getString(R.string.image_classifier_failed))
+            Log.e(TAG, e.message.toString())
+        }
+    }
+
+    fun classifyImage(image: ImageProxy) {
+
+        if (imageClassifier == null) {
+            setupImageClassifier()
+        }
+
+        val mpImage = BitmapImageBuilder(image.toBitmap()).build()
+
+        val imageProcessingOptions = ImageProcessingOptions.builder()
+            .setRotationDegrees(image.imageInfo.rotationDegrees)
+            .build()
+
+        image.close()
+
+        val inferenceTime = SystemClock.uptimeMillis()
+        imageClassifier?.classifyAsync(
+            mpImage,
+            imageProcessingOptions,
+            inferenceTime
+        )
+    }
+
+    interface ClassifierListener {
+        fun onError(error: String)
+        fun onResults(
+            results: List<Classifications>?,
+            inferenceTime: Long
+        )
+    }
+
+    companion object {
+        private const val TAG = "ImageClassifierHelper"
+    }
+}
